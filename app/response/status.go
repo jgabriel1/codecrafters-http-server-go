5049d5e31c6b4258c081f02228851747e6d20ccb
jgabriel1 <jotag.94@gmail.com>
2024-06-04 20:37:13 -0300

    add created status response

--- app/response/status.go
+++ app/response/status.go
@@ -4,6 +4,7 @@ type Status uint8
 
 const (
 	StatusOk = iota
+	StatusCreated
 	StatusNotFound
 )
 
@@ -11,6 +12,8 @@ func (s Status) ToEncoded() string {
 	switch s {
 	case StatusOk:
 		return "HTTP/1.1 200 OK"
+	case StatusCreated:
+		return "HTTP/1.1 201 Created"
 	case StatusNotFound:
 		return "HTTP/1.1 404 Not Found"
 	default:
